660b815d2bdbd14dc37109a2870a657c489afe8b
Patrick Harris <patricknpharris@gmail.com>
2018-08-24 12:05:11 +0100

    set up folders with lib and spec, init bundler and rspec

---
+++ 7kyu/lib/discover_the_original_price.rb
@@ -0,0 +1,6 @@
+def discover_original_price(sale_price, percentage)
+  # We need to write some code to return the original price of a product, the return type must be of type decimal and the number must be rounded to two decimal places.
+  #
+  # We will be given the sale price (discounted price), and the sale percentage, our job is to figure out the original price.
+  
+end
