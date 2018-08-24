b47c2cdaa4e3e28624b9a28fef7ea650c2234bd0
Patrick Harris <patricknpharris@gmail.com>
2018-08-24 15:29:32 +0100

    tested for 0 0 and empty string

--- 6kyu/spec/find_the_discounted_prices_spec.rb
+++ 6kyu/spec/find_the_discounted_prices_spec.rb
@@ -3,17 +3,22 @@ require 'find_the_discounted_prices'
 describe 'find_discounted' do
   it 'returns single discounted price' do
     expect(find_discounted('15 20')).to eq '15'
+    expect(find_discounted('0 0')).to eq '0'
   end
 
   it 'ignores incorrect single discounted price' do
     expect(find_discounted('80 100')).to eq ""
   end
 
-  it 'returns multiple discounted prices' do
+  it 'returns multiple discounted prices when provided in pairs' do
     expect(find_discounted('15 20 60 80')).to eq '15 60'
   end
 
-  it 'returns multiple discounted prices and ignores incorrect prices' do
+  it 'returns multiple and ignores incorrect prices when provided in pairs' do
     expect(find_discounted('15 20 60 80 90 100')).to eq '15 60'
+  end
+
+  it 'ignores empty string' do
+    expect(find_discounted('')).to eq ''
   end
 end
