9160f633bf0465f9b48f95fea874aeeba427f85b
Patrick Harris <patricknpharris@gmail.com>
2018-08-24 14:29:27 +0100

    ignores single discounted price

--- 6kyu/spec/find_the_discounted_prices_spec.rb
+++ 6kyu/spec/find_the_discounted_prices_spec.rb
@@ -4,4 +4,8 @@ describe 'find_discounted' do
   it 'returns single discounted price' do
     expect(find_discounted("15 20")).to eq "15"
   end
+
+  it 'ignores incorrect single discounted price' do
+    expect(find_discounted("80 100")).to eq nil
+  end
 end
