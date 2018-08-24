114b9c673db04eed415cf7c210aaedb423d3771e
Patrick Harris <patricknpharris@gmail.com>
2018-08-24 15:02:32 +0100

    returns multiple discounted prices

--- 6kyu/lib/find_the_discounted_prices.rb
+++ 6kyu/lib/find_the_discounted_prices.rb
@@ -1,7 +1,14 @@
 def find_discounted(prices)
   # https://www.codewars.com/kata/find-the-discounted-prices/train/ruby
   price_arr = prices.split(' ')
-  return price_arr[0] if verify_discounted(price_arr[0],price_arr[1])
+  discounted = []
+  (price_arr.length/2).times do
+    currently_verifying = price_arr.shift(2)
+    if verify_discounted(currently_verifying[0], currently_verifying[1])
+      discounted.push(currently_verifying[0])
+    end
+  end
+  discounted.join(' ')
 end
 
 def verify_discounted(price_1, price_2)
--- 6kyu/spec/find_the_discounted_prices_spec.rb
+++ 6kyu/spec/find_the_discounted_prices_spec.rb
@@ -2,10 +2,14 @@ require 'find_the_discounted_prices'
 
 describe 'find_discounted' do
   it 'returns single discounted price' do
-    expect(find_discounted("15 20")).to eq "15"
+    expect(find_discounted('15 20')).to eq '15'
   end
 
   it 'ignores incorrect single discounted price' do
-    expect(find_discounted("80 100")).to eq nil
+    expect(find_discounted('80 100')).to eq ""
+  end
+
+  it 'returns multiple discounted prices' do
+    expect(find_discounted('15 20 60 80')).to eq '15 60'
   end
 end
